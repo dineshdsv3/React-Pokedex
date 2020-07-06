42fd7871124475f14f544dc2869859891ecf2f1a
dineshdsv3 <dineshdsv3@gmail.com>
2020-07-06 06:22:20 +0530

    Basic react structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>Basic React Component</h1>
+    )
+  }
+
+}
+
+export default App;
